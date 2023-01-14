01bf6e8468d7432f0fad9ff60cf6c47477435ac8
Christian <winterdevelopers@gmail.com>
2023-01-14 19:42:06 +0100

    collections modified

--- next_sofa/settings.py
+++ next_sofa/settings.py
@@ -25,7 +25,7 @@ STATIC_DIR = BASE_DIR/'static'
 SECRET_KEY = 'django-insecure-+i9)9j^v60&%6h*3)qyya8!fl5qo_tva2@*%4!)fbx-v$m6p+j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['157.230.26.202','nextsofa.store','www.nextsofa.store','127.0.0.1']
 
