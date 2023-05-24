7a62553140f97b8d5ffc2c6b46ffc34e3f1a1ef5
Christian <winterdevelopers@gmail.com>
2023-05-24 10:14:17 +0100

    nexjs and drf  commit

---
+++ frontend/next_sofa/src/components/Global/loading_page.js
@@ -0,0 +1,8 @@
+export default function LoadingPage(){
+
+    return<>
+        <section className="loading_page">
+            <img src="../assets/icons/loading.svg" alt=""></img>
+        </section>
+    </>
+}
